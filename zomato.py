80d3e3ba1f410c0c549e623b46ce635b48641d6f
NishanthPalani <154805864+NishanthPalani@users.noreply.github.com>
2024-04-05 09:59:15 +0530

    Update zomato.py

--- zomato.py
+++ zomato.py
@@ -1,13 +1,14 @@
+
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 import plotly.express as px 
-#import geopandas as go
 
+## Reading the files
 zomato_df = pd.read_excel(r"https://github.com/NishanthPalani/Zomato_data_visualization/blob/main/zomato.xlsx")
 zomato_exploded_df = pd.read_excel(r"https://github.com/NishanthPalani/Zomato_data_visualization/blob/main/zomato_exploded.xlsx")
 
-## Below code will do page title(tab Name) configuration
+## Below code will set page title(tab Name) configuration
 st.set_page_config(page_title= "Zomato data Visualization",
                    layout= "wide")
 
@@ -20,20 +21,24 @@ selected = option_menu(None, ["Home","Explore More Data","Data Analysis on INDIA
             styles={"nav-link": {"font-size": "20px", "text-align": "left", "margin": "-2px", "--hover-color": "#6F36AD"},
                     "nav-link-selected": {"background-color": "#6F36AD"}})
 
+## Below code will display the home tab section of the page
 if selected == "Home":
 
     st.markdown("# Zomato Data Visualization and Exploration")
     st.write(" ")
     st.markdown("### :blue[What is Zomato? :]")
-    st.markdown("### Zomato is a popular restaurant discovery and food delivery service. It is India's largest online food ordering and delivery service. It was founded in 2010 by Arvind Narayanan and Rohit Kumar. It has been featured in Forbes, Top 100, and many other publications.")
+    st.markdown("### Zomato is an Indian multinational restaurant aggregator and food delivery company. It was founded by Deepinder Goyal and Pankaj Chaddah in 2008. Zomato provides information, menus and user-reviews of restaurants as well as food delivery options from partner restaurants in more than 1,000 Indian cities and towns, as of 2022–23. Zomato rivals Swiggy in food delivery and hyperlocal space")
+    st.write(" ")
+    st.markdown("### :blue[Problem Statement :]")
+    st.markdown("### Zomato is a popular restaurant discovery and food delivery service. Data analysis on the platform's data could be used to gain insights into customer preferences and behavior, as well as identify trends in the restaurant industry. In real world these data could be challenging to analyze and interpret. Inorder to make it easy we will use data visualization techniques that can be used effectively to communicate the insights from Zomato data analysis to a wide range of stakeholders, including restaurants, food industry players, and investors")
     st.write(" ")
     st.markdown("### :blue[Overview :]")
-    st.markdown("#### This streamlit app aims to give users a friendly environment which can be used to visualize the Zomato data and gain lots of insights on Top restaurants.")
+    st.markdown("#### This streamlit app aims to give users a friendly environment which can be used to visualize the Zomato data and gain lots of insights on Top restaurants its favourite Cuisine, Number of users using online deliveries and Dine-in. Bar charts, Pie charts and Geo map visualization are used to get better insights.")
     st.write(" ")
     st.markdown("### :blue[Technologies used :]")
     st.markdown("#### - Python, Pandas")
-    st.markdown("#### - Streamlit and Plotly")
-
+    st.markdown("#### - Streamlit and Plotly Dashboard")
+## Below code will be active if "Explore More Data" is selected from side bar where the visual charts can be seen for Analysis
 elif selected == "Explore More Data":
     st.write("### :violet[Explore More Data using interactive visuals]")
     st.write(" ")
@@ -43,13 +48,12 @@ elif selected == "Explore More Data":
 
     col1, col2 = st.columns([2,2],gap="large")
 
-    with col1:
-
+    with col1:  ## Below option will enable the select box to choose the Specific country to do the analysis
         result = st.selectbox("Please select the country to see individual analysis",zomato_df["Country"].unique(),index=0)
         df = zomato_df[zomato_df["Country"] == result]
         df_exploded = zomato_exploded_df[zomato_exploded_df["Country"] == result]
     
-    with col2:
+    with col2: ## These are some additional informations of the selected country to see more details
         total = int(df["Restaurant Name"].count())
         st.markdown(f"### :orange[Total listed Restaurants in :red[{result}]]: :green[{total}]")
         most_costly_cuisine = df_exploded.groupby('Cuisines')['INR_currency'].mean().idxmax()
@@ -58,9 +62,8 @@ elif selected == "Explore More Data":
 
     col1,col2= st.columns([2,2],gap="small")
 
-    with col1:
+    with col1: ## Below code will show the top 5 high rated restaurants in the selected country with a BAR CHART
         df1 = df[df["Aggregate rating"] == df["Aggregate rating"].max()][["Restaurant Name","Votes","Cuisines"]].sort_values(by = "Votes",ascending=False).head(5)
-        
         fig = px.bar(df1,x="Restaurant Name",y="Votes",color="Restaurant Name",hover_name="Votes",text="Cuisines",color_discrete_sequence=px.colors.sequential.Blackbody,
         labels={"Restaurant Name":"Restaurant Names","Votes":"Total Recommendations"})
         fig.update_layout(
@@ -74,9 +77,8 @@ elif selected == "Explore More Data":
         st.write(" ")
 
 
-    with col2:
+    with col2: ## Below code will show the top 8 recommended cuisines in the selected country with a BAR CHART
         df1 = df_exploded.groupby("Cuisines")[["Aggregate rating","Votes"]].mean().reset_index().sort_values(["Aggregate rating","Votes"],ascending=False).head(8)
-        
         fig = px.bar(df1,x="Aggregate rating",y="Cuisines",color="Cuisines",hover_name="Votes",text="Votes",orientation="h",color_discrete_sequence=px.colors.sequential.YlGn,
         labels={"Cuisines":"Top Cuisines","Votes":"Total Recommendations"})
         fig.update_layout(
@@ -88,7 +90,8 @@ elif selected == "Explore More Data":
         fig.update_layout(title_text="Top Recommende Cuisines in "+result, title_x=0.2,title_font_color="orange")
         st.write(fig)
         st.write(" ")
-
+    
+    ## Below code will give the heading for the Average Cost of Two ineach city and its line chart
     st.header(":rainbow[Average Cost of Two in Each city:]",divider='rainbow')
     st.markdown("Based on the chart we could see the Cost of two differes from city to city as well its location. Below it shows the Cities to the right must be of high living cost and to the left it should be of low living cost. Based on these report Zomato should always prefere giving the foods with lower cost for places where the cost of living is very low because mostly the people who live there are getting less salaries. So in those areas if they sell the product with higer cost definitely it will not be sold and it will be a loss. Whereas on the other hand where the Cost of living is higher Zomato can focus foods that are more in cost because in these places definitelty people are going to get more salaries since the cost of living is higher. So here Zomota can get good profit which can be equalized in low cost of living area")
 
@@ -103,7 +106,8 @@ elif selected == "Explore More Data":
     fig.update_layout(title_text="Average Cost of Two in Each city of "+result, title_x=0.4,title_font_color="orange")
     st.write(fig)
     st.write(" ")
-
+    
+    ## Below code will give the Bar Chart representation for the Online delivery count in each city
     df_online = df.groupby(['City', 'Has Online delivery']).size().reset_index()
     fig = px.bar(df_online,x="City",y=0,color="Has Online delivery",hover_name="City",text="Has Online delivery",labels={"City":"City Names","0":"Online Delivery count"})
     fig.update_layout(
@@ -115,7 +119,7 @@ elif selected == "Explore More Data":
     fig.update_layout(title_text="Online delivery count in Each city of "+result, title_x=0.4,title_font_color="orange")
     st.write(fig)
     
-
+    ## Below code will give the Bar Chart representation for the Cuisines count in each city
     st.write(" ")
     st.header(":rainbow[Total Cuisines list in Each city:]",divider='rainbow')
     st.markdown("We can see from the below based on people interest the Cuisines count is also increases the left most is high prefered Cuisine in the country which stands top with respect to people interest and rating. So Zomato can try to partner with the top rated cuisines and give it to the best offer ")
@@ -132,20 +136,21 @@ elif selected == "Explore More Data":
 
     col1,col2,col3= st.columns([2,2,2],gap="small")
     
+    ## Below code will show the percentage contribution of online delivery with a PIE chart in the selected country
     with col1:
         fig = px.pie(df.drop_duplicates(subset=['Restaurant Name']), names='Has Online delivery', color_discrete_sequence=px.colors.sequential.Sunset,width=300,height=300)
         fig.update_traces(marker_line_color = 'black',
                 marker_line_width = 2, opacity = 1)
         fig.update_layout(title_text="Online Delivery in "+result, title_x=0,title_font_color="orange")
         st.write(fig)
-
+    ## Below code will show the percentage contribution of online delivery with a PIE chart in the selected country    
     with col2:
         fig = px.pie(df.drop_duplicates(subset=['Restaurant Name']), names='Has Table booking', color_discrete_sequence=px.colors.sequential.Turbo,width=300,height=300)
         fig.update_traces(marker_line_color = 'black',
                 marker_line_width = 2, opacity = 1)
         fig.update_layout(title_text="Dine-in Services in "+result, title_x=0.1,title_font_color="orange")
         st.write(fig)
-
+   ## Below code will show the difference of INR currency with the actual country value with a BOX chart in the selected country    
     with col3:
         fig = px.box(df_exploded.groupby('Cuisines').agg({'INR_currency': 'mean'}).reset_index(),
                            x='Cuisines', y='INR_currency')
@@ -159,7 +164,7 @@ elif selected == "Explore More Data":
         st.write(fig)
 
     
-
+    ## Below code will give the individual analysis of the selected city
     st.write(" ")
     st.header(":rainbow[individual City analysis:]",divider='rainbow')
     st.markdown("From below analysis we will get to know the Number of listed restaurants in each city, Recommended Restaurant in City, Which Cuisines is most preferred in that city, What is the average Cost of Two people for a meal, How many online deliveries are happening, How many dine-in are happening.  ")
@@ -170,7 +175,8 @@ elif selected == "Explore More Data":
         result1 = st.selectbox(f"Cities of :red[{result}] to see individual analysis",df["City"].unique(),index=0)
         df1 = df[df["City"] == result1]
         df1_exploded = df_exploded[df_exploded["City"] == result1]
-
+    
+    ## Below code will give more Insight on each City data 
     with col2:
         total = int(df1["Restaurant Name"].count())
         st.markdown(f"##### :orange[Total listed Restaurants in :green[{result1}]]: :red[{total}]")
@@ -185,7 +191,8 @@ elif selected == "Explore More Data":
         st.write(" ")
 
     col1,col2,col3 = st.columns([2,2,2],gap="small")
-
+    ## Below code will show the percentage contribution of online delivery, Dine-in services with a PIE chart in the selected City
+    ## Along with the rating count, Favorite cuisines in the selected city
     with col1:
         df_count = df1["Rating text"].value_counts().reset_index()
         fig = px.bar(df_count,x="Rating text",y="count",color="Rating text",hover_name="count",color_discrete_sequence=px.colors.sequential.Rainbow,
@@ -225,7 +232,7 @@ elif selected == "Explore More Data":
     fig.update_layout(title_text="Top Recommende Cuisines in "+result1, title_x=0.2,title_font_color="orange")
     st.write(fig)
     st.write(" ")
-
+## Below code is only for Data on INDIA report
 else:
     st.write("### :violet[Exploring INDIA on Zomato Data using interactive visuals]")
     st.write(" ")
@@ -234,7 +241,7 @@ else:
     df_exploded = zomato_exploded_df[zomato_exploded_df["Country"] == "India"]
 
     col1, col2 = st.columns([2,2],gap="large")
-
+    ## Below code show the top 5 high rated restaurants in India with a BAR CHART view
     with col1:
         df1 = df[df["Aggregate rating"] == df["Aggregate rating"].max()][["Restaurant Name","Votes","Cuisines"]].sort_values(by = "Votes",ascending=False).head(5)
         fig = px.bar(df1,x="Restaurant Name",y="Votes",color="Restaurant Name",hover_name="Votes",text="Cuisines",color_discrete_sequence=px.colors.sequential.Blackbody,
@@ -248,7 +255,7 @@ else:
         fig.update_layout(title_text="Top High rated Restaurant's in INDIA", title_x=0.2,title_font_color="orange")
         st.write(fig)
         st.write(" ")
-
+    ## Below code show more insights on India data
     with col2:
         total = int(df["Restaurant Name"].count())
         st.markdown(f"##### :orange[Total listed Restaurants in :red[INDIA]]: :green[{total}]")
@@ -259,13 +266,12 @@ else:
         st.markdown("The Bar chart clearly shows people visits BBQ type restaurants most often as the two top most refers to the same from the list where it has more votes with good rating.")
 
     col1, col2 = st.columns([2,2],gap="large")
-    with col1:
+    with col1: ## Below wis used to segregate the heading
         st.header(":rainbow[Recommended Cuisines:]",divider='rainbow')
         st.markdown("Based on the chart we could see in INDIA the top cuisines are of other Countries. So this tells us Most of INDIANS are preffering other country Cuisines than INDIAN Cuisines. Where they want to try and explore some different/New foods than usual.")
     
-    with col2:
+    with col2: ## Below code is to show the top most recommended cuisines in India
         df1 = df_exploded.groupby("Cuisines")[["Aggregate rating","Votes"]].mean().reset_index().sort_values(["Aggregate rating","Votes"],ascending=False).head(8)
-        
         fig = px.bar(df1,x="Aggregate rating",y="Cuisines",color="Cuisines",hover_name="Votes",text="Votes",orientation="h",color_discrete_sequence=px.colors.sequential.YlGn,
         labels={"Cuisines":"Top Cuisines","Votes":"Total Recommendations"})
         fig.update_layout(
@@ -278,7 +284,7 @@ else:
         st.write(fig)
         st.write(" ")
     
-
+    ## below code will give the heading for the Average Cost of Two in India and its line chart variation
     st.header(":rainbow[Highly Spent Cities on Online Delivery:]",divider='rainbow')
     st.markdown(f"We could see :grey[New Delhi] spends more on Online Delivery than other Cities which is a good sign to Zomato in 'NEW DELHI'. Where as rest of the Cities like ':red[Kochi, Mohali]' are speding very less in case of online delivery this is where Zomato need to give more offers at some initial stage based on their need to bring in more customers")
     st.write(" ")
@@ -311,18 +317,18 @@ else:
     fig.update_layout(title_text="Highly Spent Cities on Dine-in in INDIA", title_x=0.4,title_font_color="orange")
     st.write(fig)
     
-
+    
     st.header(":rainbow[Individual city analysis:]",divider='rainbow')
     st.markdown(f"From the below we could get insights like, How Many restaurants are listed in each City, Recommended Restaurant in City, Which Cuisine is most preferred in that City, What is the average Cost of Two people for a meal, How many online Deliveries are happening, How many Dine-in are happening  ")
 
     col1, col2 = st.columns([2,2],gap="large")
 
-    with col1:
+    with col1: ## Below code is used to select specific City where we can see additional information
         result2 = st.selectbox(f"Cities of :red[INDIA] to see individual analysis",df["City"].unique(),index=0)
         df1 = df[df["City"] == result2]
         df1_exploded = df_exploded[df_exploded["City"] == result2]
 
-    with col2:
+    with col2: ## Below code will give more insight on each city data with its own positive points
         total = int(df1["Restaurant Name"].count())
         st.markdown(f"##### :orange[Total listed Restaurants in :green[{result2}]]: :red[{total}]")
         total = df1[df1["Aggregate rating"] == df1["Aggregate rating"].max()][["Cuisines","Votes","Restaurant Name"]].sort_values(by = "Votes",ascending=False).reset_index().head(1)
@@ -336,7 +342,7 @@ else:
         st.write(" ")
     
     col1, col2, col3 = st.columns([2,2,2],gap="small")
-
+    ## Below code will give the rating count, percentage contribution of table booking, Dine-in Services with a PIE chart in selected City  
     with col1:
         df_count = df1["Rating text"].value_counts().reset_index()
         fig = px.bar(df_count,x="Rating text",y="count",color="Rating text",hover_name="count",color_discrete_sequence=px.colors.sequential.Rainbow,
@@ -364,6 +370,7 @@ else:
         fig.update_layout(title_text="Online Deliveries in "+result2, title_x=0.1,title_font_color="orange")
         st.write(fig)
     
+    ## Below will show the Map view on the price range and how it scatters between Cities
     fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City", hover_data="INR_currency", width=800, height=800, zoom=2, opacity=1, color_continuous_scale="red", color_discrete_sequence=["orange", "red", "green", "blue", "purple"],title="Map view with Price range for each location")
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(title_text="Geographical representation of Price range for each location", title_x=0,title_font_color="orange")
